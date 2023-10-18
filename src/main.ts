c5d3ef26d7a86a3537dacb1f4dc276effd90b2fd
loki <648875862@qq.com>
2023-10-18 23:26:48 +0800

    feat: add filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { AaaFilter } from './aaa.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets('public', { prefix: '/static' });
+  app.useGlobalFilters(new AaaFilter());
   await app.listen(3000);
 }
 bootstrap();
